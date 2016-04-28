b7dde2b3a0ce831c39ce3cd468fc3c661e32b872
SavvasTheodorou <theodorou.savvas@gmail.com>
2016-04-28 14:06:47 +0000

    Assignment: RPS Bonus Features

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,44 +1,110 @@
-VALID_CHOICES = %w(rock paper scissors).freeze
+VALID_CHOICES = %w(r p s l o).freeze
+
+VALID_CHOICES_ACRONYM = {
+  "r" => "rock",
+  "p" => "paper",
+  "s" => "scissors",
+  "l" => "lizard",
+  "o" => "spock"
+}.freeze
+
+WINNING_COMBINATIONS = {
+  "r" => %(s l),
+  "s" => %(p l),
+  "p" => %(r o),
+  "l" => %(o p),
+  "o" => %(s r)
+}.freeze
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def win?(first, second)
-  (first == "rock" && second == "scissors") ||
-    (first == "scissors" && second == "paper") ||
-    (first == "paper" && second == "rock")
-end
-
-def display_results(player, computer)
-  if win?(player, computer)
-    prompt("You won!")
-  elsif win?(computer, player)
-    prompt("Computer won!")
-  else
-    prompt("It's a tie")
+def display_choice_message(hash_to_iterate)
+  prompt("Please choose one:")
+  hash_to_iterate.each do |k, v|
+    prompt("Choose '#{k}'' for #{v}")
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+def win_calculation(player, computer)
+  if WINNING_COMBINATIONS[player].include?(computer)
+    "You won!"
+  elsif player == computer
+    "It's a tie!"
+  else
+    "Computer won!"
+  end
+end
 
-    break if VALID_CHOICES.include?(choice)
+# Not my idea to add this. I got it from Pete's comments to others :)
+def clear_screen
+  system('clear') || system('cls')
+end
+
+# When trying to add a method to add the winning turns I was not
+# able to add the reault. It was always reseting to zero. Can you
+# please explain why?
+
+# def win_count(result)
+#   if result == "You won!"
+#     player_count += 1
+#   elsif result == "Computer won!"
+#     computer_count += 1
+#   else
+#     prompt("No point given")
+#   end
+#   prompt("Player has #{player_count} points.")
+#   prompt("Computer has #{computer_count} points.")
+# end
+
+player_count = 0
+computer_count = 0
+
+# I tried with || "or" but it was looping until both reach 5
+# Why?
+
+while player_count < 5 && computer_count < 5
+  player_choice = ''
+  player_count = player_count
+  computer_count = computer_count
+
+  loop do
+    display_choice_message(VALID_CHOICES_ACRONYM)
+    player_choice = gets.chomp
+
+    break if VALID_CHOICES_ACRONYM.key?(player_choice.downcase)
     prompt("That's not a valid choice.")
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = ''
+  computer_choice = VALID_CHOICES_ACRONYM.keys.sample
 
-  puts "You chose: #{choice}; Computer chose #{computer_choice}."
+  # Not my idea to add this. I got it from Pete's comments to others :)
+  clear_screen
 
-  display_results(choice, computer_choice)
+  prompt("You chose: #{VALID_CHOICES_ACRONYM[player_choice].capitalize}")
+  prompt("Computer chose: #{VALID_CHOICES_ACRONYM[computer_choice].capitalize}")
 
-  prompt("Do you want to play again?")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  prompt(win_calculation(player_choice, computer_choice))
+
+  # win_count(win_calculation(player_choice, computer_choice))
+
+  if win_calculation(player_choice, computer_choice) == "You won!"
+    player_count += 1
+  elsif win_calculation(player_choice, computer_choice) == "Computer won!"
+    computer_count += 1
+  else
+    prompt("No point given")
+  end
+
+  puts
+
+  prompt("Player has #{player_count} points.")
+  prompt("Computer has #{computer_count} points.")
+
+  puts
+
 end
 
 prompt("Thank you for playing.")
