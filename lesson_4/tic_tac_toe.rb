bf123dccc8b1d01958ab861f4ad80d1e0f727737
SavvasTheodorou <theodorou.savvas@gmail.com>
2016-05-02 17:50:20 +0000

    Tic Tac Toe - rubocop

--- lesson_4/tic_tac_toe.rb
+++ lesson_4/tic_tac_toe.rb
@@ -12,14 +12,18 @@
 =end
 require 'pry'
 
-INITIAL_MARKER = ' '
-PLAYER_MARKER = "X"
-COMPUTER_MARKER = "O"
+INITIAL_MARKER = ' '.freeze
+PLAYER_MARKER = "X".freeze
+COMPUTER_MARKER = "O".freeze
+WINNING_LINES = [[1, 2, 3], [1, 4, 7], [1, 5, 9]] +
+                [[2, 5, 8], [3, 6, 9], [3, 5, 7]] +
+                [[4, 5, 6], [7, 8, 9]].freeze
 
 def prompt(message)
   puts "=> #{message}"
 end
 
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   prompt "You are #{PLAYER_MARKER} computer is #{COMPUTER_MARKER}"
@@ -39,21 +43,22 @@ def display_board(brd)
   puts "    #{brd[7]}   |    #{brd[8]}    |    #{brd[9]}  "
   puts "        |         |       "
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square #{empty_squares(brd).join(", ")}: "
+    prompt "Choose a square #{empty_squares(brd).join(', ')}: "
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry please choose a number between 1 to 9"
@@ -75,17 +80,19 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [1, 4, 7], [1, 5, 9]] +
-                  [[2, 5, 8], [3, 6, 9], [3, 5, 7]] +
-                  [[4, 5, 6], [7, 8, 9]]
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-      brd[line[1]] == PLAYER_MARKER &&
-      brd[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line|
+    # if brd[line[0]] == PLAYER_MARKER &&
+    #   brd[line[1]] == PLAYER_MARKER &&
+    #   brd[line[2]] == PLAYER_MARKER
+    #   return 'Player'
+    # elsif brd[line[0]] == COMPUTER_MARKER &&
+    #       brd[line[1]] == COMPUTER_MARKER &&
+    #       brd[line[2]] == COMPUTER_MARKER
+    #   return 'Computer'
+    # end
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-      brd[line[1]] == COMPUTER_MARKER &&
-      brd[line[2]] == COMPUTER_MARKER
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
